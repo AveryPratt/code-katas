8940a82a0daaab6777e124edc521929ed6264c40
Avery Pratt <apratt91@gmail.com>
2016-12-09 12:04:01 -0800

    fixed tests

--- katas/tests_str_to_arr.py
+++ katas/tests_str_to_arr.py
@@ -1,7 +1,7 @@
 import str_to_arr
 
-assert str_to_arr.string_to_array("Robin Singh"), ["Robin", "Singh"])
-assert str_to_arr.string_to_array("CodeWars"), ["CodeWars"])
-assert str_to_arr.string_to_array("I love arrays they are my favorite"), ["I", "love", "arrays", "they", "are", "my", "favorite"])
-assert str_to_arr.string_to_array("1 2 3"), ["1", "2", "3"])
-assert str_to_arr.string_to_array(""), [""])
+assert str_to_arr.string_to_array("Robin Singh") == ["Robin", "Singh"])
+assert str_to_arr.string_to_array("CodeWars") == ["CodeWars"])
+assert str_to_arr.string_to_array("I love arrays they are my favorite") == ["I", "love", "arrays", "they", "are", "my", "favorite"])
+assert str_to_arr.string_to_array("1 2 3") == ["1", "2", "3"])
+assert str_to_arr.string_to_array("") == [""])
