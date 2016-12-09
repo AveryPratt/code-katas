5187d3a8d17897c5ee48ff336b5b2fc4e6a07ffc
Avery Pratt <apratt91@gmail.com>
2016-12-09 11:27:26 -0800

    added lightsabers kata

---
+++ katas/lightsabers.py
@@ -0,0 +1,8 @@
+"""Returns number of lightsabers owned by a programmer"""
+
+
+def howManyLightsabersDoYouOwn(programmer=""):
+    if programmer == "Zach":
+        return 18
+    else:
+        return 0
