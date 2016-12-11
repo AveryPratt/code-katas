383a813c65e6f85244263bfcc7027096c530ebc8
Avery Pratt <apratt91@gmail.com>
2016-12-10 22:00:03 -0800

    refactored

--- src/sum_terms.py
+++ src/sum_terms.py
@@ -2,10 +2,8 @@
 
 
 def series_sum(num):
-    """Returns the sum of the 0-based series x = 1/(1+n/3)"""
-    denom = 1
+    """Returns the sum of the 0-based series x = 1 / (1 + n * 3)"""
     total = 0
-    for ind in range(0, num):
+    for denom in range(1, 1 + num * 3, 3):
         total += 1 / denom
-        denom += 3
     return "{0:.2f}".format(total)
