52fe77cd18cebc64e05a8a8e4b642081fe26b02e
Avery Pratt <apratt91@gmail.com>
2017-01-19 00:19:45 -0800

    started k_primes

---
+++ src/k_primes.py
@@ -0,0 +1,42 @@
+def count_Kprimes(k, start, end):
+    last = 2 ** k
+    primes = []
+    while last < end:
+        if last > start:
+            primes.append(int(last))
+        pos = []
+        mults = [2] * k
+        
+        last = min(pos)
+        for i in range(len(pos)):
+            if last == pos[i]:
+                mults[i] = nxt_prime(mults[i])
+    return primes
+
+
+def comp_mults(mults):
+    val = 1
+    for mult in mults:
+        val *= mult
+    return val
+
+
+def nxt_prime(num):
+    import math
+    is_prime = False
+    while not is_prime:
+        num += 1
+        root = math.floor(num ** .5) + 1
+        # import pdb; pdb.set_trace()
+        stopped = False
+        for i in range(2, root):
+            if num % i == 0:
+                stopped = True
+                break
+        if not stopped:
+            is_prime = True
+    return int(num)
+
+def puzzle(s):
+    # your code 
+    pass
